4420b7956ccb5845b32fd69adc421e6392a02cc9
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-01-14 08:51:58 +0600

    Insert seed data to database

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace DatingApp.API
         {
             var key= Encoding.ASCII.GetBytes( Configuration.GetSection("AppSettings:token").Value );
             services.AddDbContext<DataContext>( x=> x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<Seed>();
             services.AddMvc();
             services.AddCors();
             services.AddScoped<IAuthRepository,AuthRepository>();
@@ -50,7 +51,7 @@ namespace DatingApp.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -69,6 +70,7 @@ namespace DatingApp.API
                     });
                 });
             }
+           // seeder.SeedUsers();
             app.UseCors( x=> x.AllowAnyHeader().AllowAnyMethod().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseAuthentication();
             app.UseMvc();
