75a72fe51bdc8f8b6be10705b33201226ba9bb25
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-01-27 11:08:05 +0600

    Create Message entity and Relationship

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -13,6 +13,7 @@ namespace DatingApp.API.Data
 
          public DbSet<Photo> Photos { get; set; }
          public DbSet<Like> Likes { get; set; }
+         public DbSet<Message> Messages { get; set; }
 
          protected override void OnModelCreating(ModelBuilder builder)
          {
@@ -28,6 +29,17 @@ namespace DatingApp.API.Data
                     .WithMany(u => u.Likee)
                     .HasForeignKey( u => u.LikeeId)
                     .OnDelete(DeleteBehavior.Restrict);
+            
+              builder.Entity<Message>()
+                     .HasOne(u => u.Sender)
+                     .WithMany(u => u.MessagesSent)
+                     .OnDelete(DeleteBehavior.Restrict);
+
+              builder.Entity<Message>()
+                     .HasOne(u => u.Recipient)
+                     .WithMany(u => u.MessagesReceived)
+                     .OnDelete(DeleteBehavior.Restrict);
+               
          }
     }
 }   
